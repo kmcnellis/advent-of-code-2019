38e72dbd9a99ba004b9d3aa8a7d1825a77f98e56
Kiana McNellis <kianam@google.com>
2019-12-24 21:33:18 -0500

    Day 16 part 2

--- day16/main.go
+++ day16/main.go
@@ -6,14 +6,15 @@ import (
 	// "bufio"
 	"log"
 	// "strings"
-	// "math"    // Math library with local alias m.
+	// m "math"    // Math library with local alias m.
 	// "os" // OS functions like working with the file system
 	// "regexp"
 	"strconv" // String conversions.
 	"strings"
-	// "math"
+	// "sync"
 )
 
+var base = [4]int{0, 1, 0, -1}
 
 func main() {
 	// Open input file
@@ -32,33 +33,41 @@ func main() {
 
 	fmt.Printf("Initial: %+v\n", initialInput)
 
-  input := initialInput
+	// Part 1
+	// input := initialInput
+  // for i := 0;  i<5; i++ {
+  //   input = runPhase(input)
+  // 	fmt.Printf("Phase %d: %+v\n", i, input)
+  // }
+	// fmt.Printf("After 100 Phases: %+v\n", input[:8])
+
+
+	// Part 2
+	input := make([]int, 0, len(initialInput) * 10000)
+	offset, _ := strconv.Atoi(inputString[:7])
+
+  for i := 0;  i<10000; i++ {
+    input = append(input, initialInput...)
+  }
+	fmt.Printf("Initial length: %+v\n", len(initialInput))
+	fmt.Printf("Offset: %+v\n", offset)
+	fmt.Printf("Calc: %+v\n", len(input)-offset)
+	fmt.Printf("Final length: %+v\n", len(input))
+	// fmt.Printf("Initial %d: %+v\n", 0, input)
 
   for i := 0;  i<100; i++ {
-    input = runPhase(input)
-  	// fmt.Printf("Phase %d: %+v\n", i, input)
+    input = runPhaseTail(input, offset-10)
+  	fmt.Printf("Phase %d: %+v\n", i, input[offset:offset+8])
   }
-	fmt.Printf("After 100 Phases: %+v\n", input[:8])
-
+	fmt.Printf("After 100 Phases: %+v\n", input[offset:offset+8])
 }
 
-func generatePattern(outputNum int, length int) ([]int){
-  pattern := make([]int, length+1)
-  base := [4]int{0, 1, 0, -1}
 
-  basePart := 0
-  baseNum := 0
-  for i := 0;  i<length+1; i++ {
-    pattern[i] = base[basePart]
-    baseNum+=1
-    if baseNum >= outputNum{
-      basePart += 1
-      basePart = basePart % 4
-      baseNum = 0
-    }
-  }
-
-  return pattern[1:]
+func getPattern(inputPos int, outputPos int) (int){
+	repeat := outputPos*4
+	pos := inputPos % repeat
+	offset := pos / outputPos // int division uses floor
+	return base[offset]
 }
 
 func runPhase(input []int) ([]int) {
@@ -79,6 +88,28 @@ func runPhase(input []int) ([]int) {
     output[i] = output[i]  % 10
   	// fmt.Printf("= %d\n", output[i])
 
+  }
+
+  return output
+}
+
+// For everything in the last half of the digits, the value for position P
+// each iteration is output[P] = input[P] + input[P+1] + input[P+2]....
+// this simplifies to input[P]+output[P+1]
+func runPhaseTail(input []int, start int) ([]int) {
+  output := make([]int, len(input))
+
+  for i := 1;  i < len(input)-start; i++ {
+		index := len(input) - i
+		if (i == 1){
+			output[index] = input[index]
+		} else {
+	    output[index] = input[index] + output[index+1]
+		}
+    if output[index] < 0 {
+      output[index] *= -1
+    }
+    output[index] = output[index]  % 10
   }
 
   return output
